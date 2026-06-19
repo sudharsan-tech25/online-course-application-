c2a9a33f19723b145aec479c6970cad8dbec740f
sudharsan-tech25 <sudharsan9225@gmail.com>
2026-06-19 22:32:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -19,6 +19,18 @@ function showMessage() {
 }
 function registerMessage() {
 
+    let name = document.getElementById("name").value;
+
+    let email = document.getElementById("registerEmail").value;
+
+    let password = document.getElementById("registerPassword").value;
+
+    localStorage.setItem("name", name);
+
+    localStorage.setItem("email", email);
+
+    localStorage.setItem("password", password);
+
     alert("Registration Successful!");
 
 }
