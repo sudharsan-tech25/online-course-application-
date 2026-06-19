057dc5c7ff703e8a7f2fd8c37cb87e07540392ac
sudharsan-tech25 <sudharsan9225@gmail.com>
2026-06-19 22:29:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 function showMessage() {
 
-    alert("Login Successful!");
+    let email = document.getElementById("email").value;
+
+    let password = document.getElementById("password").value;
+
+    if(email === "admin@gmail.com" && password === "1234") {
+
+        alert("Login Successful!");
+
+    }
+
+    else {
+
+        alert("Invalid Email or Password!");
+
+    }
 
 }
 function registerMessage() {
