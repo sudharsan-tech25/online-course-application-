44cdb046af68d80ab15dff3e8b1309600e34fd78
sudharsan-tech25 <sudharsan9225@gmail.com>
2026-06-23 10:53:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -228,3 +228,35 @@ function sendMessage() {
     }
 
 }
+let images = [
+
+    "banner1.jpg",
+
+    "banner2.jpg",
+
+    "banner3.jpg"
+
+];
+
+let index = 0;
+
+setInterval(function() {
+
+    let slide =
+    document.getElementById("slideImage");
+
+    if(slide) {
+
+        index++;
+
+        if(index >= images.length) {
+
+            index = 0;
+
+        }
+
+        slide.src = images[index];
+
+    }
+
+}, 3000);
